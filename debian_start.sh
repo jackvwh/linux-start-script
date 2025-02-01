7c61463514a498046db3f65f18687a04b821541f
Jack <jackvwh@hotmail.com>
2025-02-01 15:28:10 +0100

    Improve debian_start.sh by adding conditional installations for git, gh cli, and VS Code to streamline setup process

--- debian_start.sh
+++ debian_start.sh
@@ -6,8 +6,22 @@ echo "Starting the script with user: $USER and sudo user: $SUDO_USER"
 echo "Updating the system..."
 sudo apt update -y && sudo apt upgrade -y
 
-# Install development essentials
-sudo apt install git gh build-essential nodejs npm code -y
+# Install nodejs and npm
+echo "Installing Node.js and npm..."
+sudo apt install nodejs npm -y
+
+# Install git and gh cli
+if ! command -v git &> /dev/null
+then
+    echo "Installing git..."
+    sudo apt install git -y
+fi
+
+if ! command -v gh &> /dev/null
+then
+    echo "Installing gh cli..."
+    sudo apt install gh -y
+fi
 
 # Set git config
 git config --global user.name "Jack Hansen"
@@ -16,6 +30,22 @@ git config --global push.autoSetupRemote true
 git config --global gpg.format ssh 
 git config --global user.signingkey ~/.ssh/id_ed25519
 git config --global commit.gpgsign true 
+
+
+# Install vscode
+if ! command -v code &> /dev/null
+then
+    echo "Installing VS Code..."
+    sudo apt-get install wget gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+    echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    rm -f packages.microsoft.gpg
+
+    sudo apt install apt-transport-https
+    sudo apt update
+    sudo apt install code
+fi
 
 # Install Terraform
 if ! command -v terraform &> /dev/null
