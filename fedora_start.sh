fc3f2b273edbd7f3c88c5b13e8bc0ad9d38bd8f2
Jack <jackvwh@hotmail.com>
2025-02-02 14:33:33 +0100

    Remove multimedia codec installation commands from fedora_start.sh to streamline setup process and reduce redundancy

--- fedora_start.sh
+++ fedora_start.sh
@@ -113,10 +113,6 @@ then
     curl -fsS https://dl.brave.com/install.sh | sh
 fi
 
-# install Multimedia Codecs
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
-sudo dnf install lame\* --exclude=lame-devel -y
-sudo dnf group upgrade --with-optional Multimedia -y
 
 # Install Slack flathub 
 if ! command -v slack &> /dev/null
