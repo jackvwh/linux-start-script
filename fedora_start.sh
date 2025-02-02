ba9c4fe93b9cbce8fa86dc80789083162e542cdf
Jack <jackvwh@hotmail.com>
2025-02-02 15:22:03 +0100

    Enable Docker service on install in fedora_start.sh

--- fedora_start.sh
+++ fedora_start.sh
@@ -96,6 +96,7 @@ then
     sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
     sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+    sudo systemctl enable --now docker
     sudo docker run hello-world
 
 # linux post install docker
