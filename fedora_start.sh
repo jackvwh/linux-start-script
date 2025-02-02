bc2392e5eef9155ace99c5ebf2fb70e1049ab6db
Jack Hansen <jackvwh@hotmail.com>
2025-02-02 16:04:23 +0100

    Setup SSH folder and key files in fedora_start.sh for secure connections

--- fedora_start.sh
+++ fedora_start.sh
@@ -138,6 +138,12 @@ then
     fi
 fi
 
+# Setup SSH folder and id_ed25519 files
+mkdir -p ~/.ssh
+touch ~/.ssh/id_ed25519
+touch ~/.ssh/id_ed25519.pub
+chmod 600 ~/.ssh/* && chmod 700 ~/.ssh && chmod 644 ~/.ssh/*.pub
+
 # set hostname
 sudo hostnamectl set-hostname "mole"
 
